917a8db0b2049b9a9f2b01e269c59681a0272f3d
Idel-Alves <idel.alves97@gmail.com>
2024-08-21 14:48:57 -0300

    Desafio FocusTimer Stage 05 - Avançando no JavaScript

---
+++ JS/FocusTimer/sounds.js
@@ -0,0 +1,7 @@
+export const buttonPressAudio = new Audio('./ASSETS/button-press.wav');
+
+export const kitchenTimer = new Audio('./ASSETS/kichen-timer.mp3');
+
+export const bgAudio = new Audio('./ASSETS/bg-audio.mp3');
+
+bgAudio.loop = true;
